631bf00088acf956edf741b56420558fc2d1f6bc
xiaoshijiu <mychenshan@126.com>
2020-06-16 15:31:38 +0800

    添加判断文件名是否合规的方法

--- src/main/java/com/fei/common/file/FileNameFarmat.java
+++ src/main/java/com/fei/common/file/FileNameFarmat.java
@@ -1,5 +1,8 @@
 package com.fei.common.file;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @Author: xiaoshijiu
  * @Date: 2020/6/13
@@ -7,13 +10,28 @@ package com.fei.common.file;
  */
 public class FileNameFarmat {
 
+    private static Pattern pattern = Pattern.compile("<|>|:|\"|\\*|\\?|\\/|\\\\|\\|");
+
     /**
      * @param filename 文件名
      * @param to 将这些字符转成什么
+     * @return 替换不合规字符之后的重命名
      */
     public static String renameFile(String filename, String to) {
         // 反斜杠在Java里面表示转义符，在正则表达式里面也表示转义符，所以在replaceAll中四个反斜杠表示一个反斜杠
         return filename.replaceAll("<|>|:|\"|\\*|\\?|\\/|\\\\|\\|", to);
+    }
+
+    /**
+     * @param filename 文件名
+     * @return 是否不合规，即包含了特殊字符
+     */
+    public static boolean fileNameStandard(String filename) {
+        Matcher matcher = pattern.matcher(filename);
+        if (matcher.find()) {
+            return true;
+        }
+        return false;
     }
 
     /**
